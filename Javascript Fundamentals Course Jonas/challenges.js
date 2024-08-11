c6f8f2951773d79686c2dc99db5ae153b97685df
Tapiwa <tapiwachimbwanda4@gmail.com>
2024-08-11 08:56:50 -0400

    completed fundamentals part 1

--- Javascript Fundamentals Course Jonas/challenges.js
+++ Javascript Fundamentals Course Jonas/challenges.js
@@ -1,4 +1,4 @@
-// Challenge 1 Solution
+/* // Challenge 1 Solution
 
 let heightMark = 1.69;
 let heightJohn = 1.95;
@@ -16,3 +16,46 @@ if (markHigherBMI) {
 } else {
   console.log(` John's BMI ${BMIJohn} is higher than  Marks's BMI${BMIMark} `);
 }
+ */
+
+/* //challenge 3
+const avgDolphins = (96 + 108 + 89) / 3;
+const avgKoala = (88 + 91 + 110) / 3;
+
+if (avgDolphins > avgKoala) {
+  console.log(`Dolphins are the winners`);
+} else if (avgDolphins < avgKoala) {
+  console.log(`Kolas are the winners`);
+} else {
+  console.log("It is a draw");
+}
+
+//challenge 3 bonus 1 and 2
+const avgDolphins1 = (97 + 112 + 101) / 3;
+const avgKoala1 = (109 + 95 + 106) / 3;
+
+if (avgDolphins1 > avgKoala1 && avgDolphins1 > 100) {
+  console.log(`Dolphins1 are the winners`);
+} else if (avgDolphins1 < avgKoala1 && avgKoala1 > 100) {
+  console.log(`Koalas1 are the winners`);
+} else if (avgDolphins1 === avgKoala1 && avgDolphins1 > 100) {
+  console.log("It is a draw bonus 2");
+} else {
+  console.log("No one has won the trophy");
+}
+ */
+
+// challege 4
+
+const bill = 430;
+let tip;
+
+bill >= 50 && bill <= 300
+  ? console.log(
+      `The bill was ${bill}, Tip is (15%): ${(tip =
+        bill * 0.15)}, and the total value is ${bill + tip}`
+    )
+  : console.log(
+      `The bill was ${bill}, Tip is (20%): ${(tip =
+        bill * 0.2)}, and the total value is ${bill + tip}`
+    );
