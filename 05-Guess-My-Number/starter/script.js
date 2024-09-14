fd1d63c4ab2b81c07f31413dfd247d7a4f454e20
Tapiwa <tapiwachimbwanda4@gmail.com>
2024-09-14 03:23:41 +0300

    Started new project - Pig Game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,3 +1,29 @@
 'use strict';
 
-document.querySelector('score').textContent = 70;
+let secNumber = (document.querySelector('.number').textContent = Math.trunc(
+  Math.random() * 20 + 1
+));
+let score = (document.querySelector('.score').textContent = 60);
+
+document.querySelector('.check').addEventListener('click', function () {
+  const input = Number(document.querySelector('.guess').value);
+  score--;
+
+  if (!input) {
+    document.querySelector('.message').textContent = 'No Number';
+  } else if (input === secNumber) {
+    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('.message').textContent = 'Congratulations';
+    document.querySelector('.number').style.width = '30rem';
+  } else if (input < secNumber) {
+    document.querySelector('.message').textContent = 'number too low';
+    score -= 1;
+  } else if (input > secNumber) {
+    document.querySelector('.message').textContent = 'number too high';
+    score--;
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('body').style.backgroundColor = 'red';
+});
